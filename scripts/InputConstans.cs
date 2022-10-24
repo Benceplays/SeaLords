e0c6fc9bed9643cfc1989fa320649738d907536d
Németh Csaba Bence <71324001+Benceplays@users.noreply.github.com>
2022-10-24 19:55:07 +0200

    par cucc

--- scripts/InputConstans.cs
+++
@@ -1,7 +0,0 @@
-class InputConstans{
-    public const string move_right = "move_right";
-    public const string move_forward = "move_forward";
-    public const string move_backward = "move_backward";
-    public const string move_left = "move_left";
-
-}
--- scripts/movement.cs
+++ scripts/movement.cs
@@ -1,6 +1,14 @@
 using Godot;
 using System;
 
+class InputConstans{
+    public const string move_right = "move_right";
+    public const string move_forward = "move_forward";
+    public const string move_backward = "move_backward";
+    public const string move_left = "move_left";
+
+}
+
 public class movement : Sprite
 {
     public override void _Ready()
@@ -10,11 +18,18 @@ public class movement : Sprite
     public override void _PhysicsProcess(float delta){
         Vector2 Move = new Vector2(0, 0);
         float speed = 50;
-        if (Input.IsActionPressed(InputConstans.move_left)){Move.x -= 1;}
-        if (Input.IsActionPressed(InputConstans.move_backward)){Move.y += 1;}
+        if (Input.IsActionPressed(InputConstans.move_left)){Move.x -= 1; Rotation = 9.44f;}
+        if (Input.IsActionPressed(InputConstans.move_backward)){Move.y += 1; Rotation = 7.85f;}
+        if (Input.IsActionPressed(InputConstans.move_forward)){Move.y -= 1; Rotation = 4.7f;}
+        if (Input.IsActionPressed(InputConstans.move_right)){Move.x += 1; Rotation = 6.3f;}
+        /*
+        probalkozas
+        if (Input.IsActionPressed(InputConstans.move_left)){Rotation -= 0.0001f * speed;}
+        if (Input.IsActionPressed(InputConstans.move_backward)){Move.y += 0.2f;}
         if (Input.IsActionPressed(InputConstans.move_forward)){Move.y -= 1;}
-        if (Input.IsActionPressed(InputConstans.move_right)){Move.x += 1;}
-        GlobalPosition += Move*speed*delta;
+        if (Input.IsActionPressed(InputConstans.move_right)){Rotation += 0.0001f * speed;}
+        */
+                GlobalPosition += Move*speed*delta;
     }
 
 }
