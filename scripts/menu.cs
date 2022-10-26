8889f805dadd8eb0890934b97706fd55cbf2dca6
BalintKollar852 <balintkollar852@gmail.com>
2022-10-26 19:18:56 +0200

    asd

--- scripts/menu.cs
+++ scripts/menu.cs
@@ -1,24 +1,22 @@
 using Godot;
 using System;
 
-public class menu : Node
+public class menu : Node2D
 {
     public override void _Ready()
     {
         
     }
-    private void _on_play_pressed(){
+    private void _on_Play_pressed(){
         GetTree().ChangeScene("res://Game.tscn");
     }
-    private void _on_hazi_pressed(){
-        GD.Print("asd");
+    private void _on_Maps_pressed(){
         var changedlabel = "És megváltozott a szoveg.";
-        var label = GetNode("../text") as Label;
+        var label = GetNode("text") as Label;
         label.Text = changedlabel;
     }
-    private void _on_exit_pressed(){
-        GD.Print("exit");
-        GetTree().Quit();
+    private void _on_Exit_pressed(){
+        GetTree().Quit();   
     }
 
 }
--- scripts/movement.cs
+++ scripts/movement.cs
@@ -1,35 +1,38 @@
 using Godot;
 using System;
 
-class InputConstans{
-    public const string move_right = "move_right";
-    public const string move_forward = "move_forward";
-    public const string move_backward = "move_backward";
-    public const string move_left = "move_left";
-
-}
-
-public class movement : Sprite
+public class movement : KinematicBody2D
 {
-    public override void _Ready()
+    [Export] public int speed = 200;
+    [Export] public float rotationSpeed = 1.5f;
+
+    public Vector2 velocity = new Vector2();
+    public int rotationDir = 0;
+
+    public void GetInput()
     {
-        
-    }
-    public override void _PhysicsProcess(float delta){
-        Vector2 Move = new Vector2(0, 0);
-        float speed = 50;
-        if (Input.IsActionPressed(InputConstans.move_left)){Move.x -= 1; Rotation = 9.44f;}
-        if (Input.IsActionPressed(InputConstans.move_backward)){Move.y += 1; Rotation = 7.85f;}
-        if (Input.IsActionPressed(InputConstans.move_forward)){Move.y -= 1; Rotation = 4.7f;}
-        if (Input.IsActionPressed(InputConstans.move_right)){Move.x += 1; Rotation = 6.3f;}
-        /*
-        probalkozas
-        if (Input.IsActionPressed(InputConstans.move_left)){Rotation -= 0.0001f * speed;}
-        if (Input.IsActionPressed(InputConstans.move_backward)){Move.y += 0.2f;}
-        if (Input.IsActionPressed(InputConstans.move_forward)){Move.y -= 1;}
-        if (Input.IsActionPressed(InputConstans.move_right)){Rotation += 0.0001f * speed;}
-        */
-                GlobalPosition += Move*speed*delta;
+        rotationDir = 0;
+        velocity = new Vector2(); 
+
+        if (Input.IsActionPressed("right"))
+            rotationDir += 1;
+
+        if (Input.IsActionPressed("left"))
+            rotationDir -= 1;
+
+        if (Input.IsActionPressed("down"))
+            velocity = new Vector2(-speed, 0).Rotated(Rotation);
+
+        if (Input.IsActionPressed("up"))
+            velocity = new Vector2(speed, 0).Rotated(Rotation);
+
+        velocity = velocity.Normalized() * speed;
     }
 
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        Rotation += rotationDir * rotationSpeed * delta;
+        velocity = MoveAndSlide(velocity);
+    }
 }
